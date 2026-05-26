a5e9117ec87741665165bd1f07019674beecf25c
pureferret <pureferret@gmail.com>
2026-05-26 18:21:26 +0100

    chore(automation): add commitlint, husky and commit lint config
    BREAKING CHANGE: Can't name a commit anything you want anymore

---
+++ commitlint.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // TODO customise
+    // 'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'refactor', 'chore', 'ci', 'test']],
+  },
+};
