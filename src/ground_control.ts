e6ff12c4594d6b1aef3360ae2dc2e458d8376aca
pureferret <pureferret@gmail.com>
2026-06-13 10:19:15 +0200

    bugfix(orbit): Display phi correctly

--- src/ground_control.ts
+++ src/ground_control.ts
@@ -6,7 +6,6 @@ import { getInteractions } from './interactions_calc.ts';
 import { getFullOrbits } from './orbit_calc.ts';
 import { standardAstroUnits, standardiseSystem } from './utils.ts';
 import { describeMathJsValue, unitToString } from './utils/mathjsUtils.ts';
-import type { OrbitalPosition } from './orbit.types.ts';
 
 const astroMath = getAstroMath();
 
